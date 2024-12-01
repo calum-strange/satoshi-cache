53a4635adf334f0d36cb5dc0ca109838d1281c01
Calum Strange <calum.str@gmail.com>
2024-12-01 12:30:12 +0000

    add definitions for binance file columns

---
+++ satoshi_cache/source/exchanges/binance/file_columns.py
@@ -0,0 +1,84 @@
+agg_trade_columns = [
+    "tradeId",
+    "price",
+    "quantity",
+    "first_tradeId",
+    "last_tradeId",
+    "timestamp",
+    "maker_bought",
+    "is_best_match",
+]
+
+klines_columns = [
+    "open_time",
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume",
+    "close_time",
+    "quote_asset_volume",
+    "number_of_trades",
+    "taker_buy_base_asset_volume",
+    "taker_buy_quote_asset_volume",
+    "ignore",
+]
+
+trade_columns = [
+    "tradeId",
+    "price",
+    "quantity",
+    "first_tradeId",
+    "last_tradeId",
+    "timestamp",
+    "maker_bought",
+    "is_best_match",
+]
+
+usd_m_klines_columns = [
+    "open_time",
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume",
+    "close_time",
+    "quote_asset_volume",
+    "number_of_trades",
+    "taker_buy_base_asset_volume",
+    "taker_buy_quote_asset_volume",
+    "ignore",
+]
+
+coin_m_klines_columns = [
+    "open_time",
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume",
+    "close_time",
+    "base_asset_volume",
+    "number_of_trades",
+    "taker_buy_quote_asset_volume",
+    "taker_buy_base_asset_volume",
+    "ignore",
+]
+
+usd_m_trade_columns = [
+    "tradeId",
+    "price",
+    "quantity",
+    "quote_asset_quantity",
+    "timestamp",
+    "maker_bought",
+]
+
+coin_m_trade_columns = [
+    "tradeId",
+    "price",
+    "quantity",
+    "base_asset_quantity",
+    "timestamp",
+    "maker_bought",
+]
